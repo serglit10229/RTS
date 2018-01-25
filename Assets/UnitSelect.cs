8a6364491ffd0f217109444be9e574dedab3689f
serglit1650 <direct.serglit@gmail.com>
2018-01-24 17:26:46 -0800

    Single Selection

--- Assets/UnitSelect.cs
+++ Assets/UnitSelect.cs
@@ -6,7 +6,9 @@ public class UnitSelect : MonoBehaviour {
 
 	Camera cam;
 	public GameObject selectionCirclePrefab;
-	public bool selected = false;
+	public bool objectSelected = false;
+	SelectableUnitComponent selectableObject;
+	//public bool selected = false;
 	//public GameObject prevSelect; 
 
 
@@ -25,38 +27,24 @@ public class UnitSelect : MonoBehaviour {
 
 			if (Physics.Raycast (ray, out hit)) 
 			{
+				selectableObject = hit.collider.GetComponent<SelectableUnitComponent> ();
 				//Debug.Log ("HIT");
 				if (hit.collider.tag == "SelectableUnit") {
 					//Debug.Log ("SelectableUnit");
-					SelectableUnitComponent selectableObject = hit.collider.GetComponent<SelectableUnitComponent> ();
+					//SelectableUnitComponent selectableObject = GetComponent(SelectableUnitComponent);
+					selectableObject.selected = true;
+					objectSelected = true;
 
-					if (selected == false) {
-						//Debug.Log ("Foreach");
-						selectableObject.selectionCircle = Instantiate (selectionCirclePrefab);
-						selectableObject.selectionCircle.transform.SetParent (selectableObject.transform, false);
-						selectableObject.selectionCircle.transform.eulerAngles = new Vector3 (90, 0, 0);
-						selected = true;
-
-						//prevSelect = selectableObject;
-					}
-					if (selected == true ) {
-						if (Input.GetMouseButtonDown (0)) 
-						{
-							Destroy (selectableObject.selectionCircle.gameObject);
-							selectableObject.selectionCircle = null;
-							selected = false;
-						}
-					}
 				} 
-				/*
-				else 
+				if (Input.GetMouseButtonDown (0)) 
 				{
-					SelectableUnitComponent selectableObject = hit.collider.GetComponent<SelectableUnitComponent> ();
-					//Destroy (prevSelect.selectionCircle.gameObject);
-					//prevSelect.selectionCircle = null;
-				}*/
+					
+					if (objectSelected == true) {
+						//SelectableUnitComponent selectableObject = GetComponent(SelectableUnitComponent) ();
+						selectableObject.Deselect ();
+					}
+				}
 			}
-
 		}
 	}
 }
